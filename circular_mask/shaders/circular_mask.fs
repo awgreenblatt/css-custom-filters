2ac4ba03bb34b8dec20490bc58e09f2f53823431
Alan Greenblatt <alangre@gmail.com>
2013-05-28 22:56:57 -0400

    minor cleanup in fragment shader

--- circular_mask/shaders/circular_mask.fs
+++ circular_mask/shaders/circular_mask.fs
@@ -12,10 +12,9 @@ varying vec2 v_uv;
 
 void main()
 {
-	float transparency = opacity;
 	float featherStep = smoothstep(radius, radius+feather, length(v_uv - center));
-	transparency = opacity + (1.0 - opacity) * featherStep;
-	css_ColorMatrix = mat4( 1.0, 0.0, 0.0, 0.0,
+	float transparency = opacity + (1.0 - opacity) * featherStep;
+	css_ColorMatrix = mat4(1.0, 0.0, 0.0, 0.0,
                         0.0, 1.0, 0.0, 0.0,
                         0.0, 0.0, 1.0, 0.0,
                         0.0, 0.0, 0.0, transparency);
