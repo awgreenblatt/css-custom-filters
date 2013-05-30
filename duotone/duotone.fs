5f1a27ba1b317d58518bf4ceccda3fddc51eb7d6
Alan Greenblatt <alangre@gmail.com>
2013-05-30 01:28:13 -0400

    Minor cleanup of duoton fragment shader

--- duotone/duotone.fs
+++ duotone/duotone.fs
@@ -9,12 +9,10 @@ void main()
 {
   vec3 b = black / 255.0;
   vec3 w = white / 255.0;
-  //b = black;
-  //w = white;
   
   css_ColorMatrix = mat4(w.r-b.r, 0.0, 0.0, 0.0,
-							0.0, w.g - b.g, 0.0, 0.0,
-							0.0, 0.0, w.b - b.b, 0.0,
-							b.r, b.g, b.b, 1.0);
+                         0.0, w.g - b.g, 0.0, 0.0,
+                         0.0, 0.0, w.b - b.b, 0.0,
+                         b.r, b.g, b.b, 1.0);
 }
 
